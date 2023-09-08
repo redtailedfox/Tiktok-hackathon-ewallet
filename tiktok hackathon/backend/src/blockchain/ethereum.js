40e6c3ec1699e121f4c2658c28daa41b8d676621
redtailedfox <hongwei.hhw@gmail.com>
2023-09-08 21:34:19 +0800

    mvp

--- tiktok hackathon/backend/src/blockchain/ethereum.js
+++ tiktok hackathon/backend/src/blockchain/ethereum.js
@@ -1,79 +1,40 @@
+const express = require('express');
+
 const { ethers } = require('ethers');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
 // Define the contract ABI as a human-readable string
 const contractABI = [
-  'function deposit() public payable',
-  'function withdraw(uint256 amount) public',
+  'function deposit(uint256 amount) public',
+  'function withdraw(uint256 amount) public'
   // Add other function declarations as needed
-].join(';');
+]
 
-const contractAddress = '0x0D54602E93aD2B38d7929d9099f22162131107c2'; // Replace with your contract's address
-const privateKey = 'YourPrivateKey'; // Replace with your wallet's private key
-const infuraUrl = 'https://mainnet.infura.io/v3/YourInfuraProjectId'; // Replace with your Infura project URL
-
-async function sendTransaction() {
-  try {
-    // Connect to the Ethereum network using Infura
-    const provider = new ethers.JsonRpcProvider(infuraUrl);
-
-    // Create a wallet from the private key
-    const wallet = new ethers.Wallet(privateKey, provider);
-
-    // Create a contract instance
-    const contract = new ethers.Contract(contractAddress, contractABI, wallet);
-
-    // Get the gas price and estimate gas limit
-    const gasPrice = await provider.getGasPrice();
-    const gasLimit = 200000; // Adjust based on your contract's requirements
-
-    // Specify the value to send in Wei
-    const valueToSend = ethers.utils.parseEther('1'); // 1 Ether in Wei
-
-    // Get the nonce for the sender's address
-    const nonce = await wallet.getTransactionCount();
-
-    // Create a transaction object
-    const tx = {
-      to: contractAddress,
-      gasPrice: gasPrice.toHexString(),
-      gasLimit: gasLimit,
-      nonce: nonce,
-      value: valueToSend.toHexString(),
-      data: contract.interface.encodeFunctionData('someMethod'), // Replace with your contract method
-    };
-
-    // Sign and send the transaction
-    const signedTx = await wallet.signTransaction(tx);
-    const txResponse = await provider.sendTransaction(signedTx);
-
-    console.log('Transaction Hash:', txResponse.hash);
-  } catch (error) {
-    console.error('Error:', error);
-  }
-}
-
-// Call the function to send a transaction
-sendTransaction();
-
-// Create a contract instance for interaction
-const provider = new ethers.JsonRpcProvider(infuraUrl);
-const contract = new ethers.Contract(contractAddress, contractABI, provider);
+const contractAddress = '0x8Eb413920478042C026610362b5D286B220Ee896'; // Replace with your contract's address
 
 async function interactWithContract() {
   try {
+    // Connect to the Ethereum network using Infura
+    const provider = new ethers.JsonRpcProvider('https://eth-sepolia.g.alchemy.com/v2/Rr3GR128NGutd1QdUlGF0-D_i5ZkTgrF');
+
+    // Create a contract instance
+    const contract = new ethers.Contract(contractAddress, contractABI, provider);
+    
+    const metamaskProvider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await metamaskProvider.getSigner();
     // Call a function on the smart contract
-    const result = await contract.someFunction();
+    const connectedcontract = contract.connect(signer)
+    const result = await connectedcontract.deposit(88);
     console.log('Result:', result);
 
-    // Send a transaction to the smart contract
-    const walletWithProvider = new ethers.Wallet(privateKey, provider);
-    const tx = await walletWithProvider.sendTransaction({
-      to: contractAddress,
-      value: ethers.utils.parseEther('1'), // 1 Ether in Wei
-      data: contract.interface.encodeFunctionData('someFunction'), // Replace with your contract method
-    });
+    // You can handle the result here as needed
 
-    console.log('Transaction Hash:', tx.hash);
   } catch (error) {
     console.error('Error:', error);
   }
@@ -81,3 +42,8 @@ async function interactWithContract() {
 
 // Call the function to interact with the contract
 interactWithContract();
+
+app.listen(port, () => {
+  console.log(`Express app listening at http://localhost:${port}`);
+});
+
